28ee7f9f0e44ee0ed53b6c8c17c53a988f7d7617
dufresnefu <dufresnefu@tencent.com>
2023-03-13 22:07:17 +0800

    win64 DS打包文件, 先删除后拷贝

--- CopyServerExe.bat
+++ CopyServerExe.bat
@@ -1,9 +1,16 @@
 
 
-set SrcDir="E:\Documents\WorkDir\ALSV\ALSV"
-set TargetDir="E:\Documents\WorkDir\ALSV_Win64PackageList"
+set SrcDir=E:\Documents\WorkDir\ALSV\ALSV
+set TargetDir=E:\Documents\WorkDir\ALSV_Win64PackageList
 
-set TargetPath=%TargetDir%"\WindowsNoEditor\ALSV\Binaries\Win64\"
+set TargetPath=%TargetDir%\WindowsNoEditor\ALSV\Binaries\Win64\
+
+del /a /f %TargetPath%\ALSVServer.exe 
+del /a /f %TargetPath%\ALSVServer.exp 
+del /a /f %TargetPath%\ALSVServer.lib 
+del /a /f %TargetPath%\ALSVServer.pdf 
+del /a /f %TargetPath%\ALSVServer.target
+
 copy %SrcDir%\Binaries\Win64\ALSVServer.exe %TargetPath%
 copy %SrcDir%\Binaries\Win64\ALSVServer.exp %TargetPath%
 copy %SrcDir%\Binaries\Win64\ALSVServer.lib %TargetPath%
